f41187c714156d9a0c92ae7c8edc6280338221a1
Shweta Kumari Shah <157615234+shwetashah791@users.noreply.github.com>
2024-08-25 20:43:13 +0530

    Time: 6 ms (79.82%), Space: 8.9 MB (41.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>m1;
-        unordered_map<char,int>m2;
-        if(s.size()!=t.size()){
-            return false;
-        }
-        for( int i=0;i<s.size();i++){
-            m1[s[i]]++;
-            m2[t[i]]++;
-        }
-        if(m1 == m2) return true;
-        return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
