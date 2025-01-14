0c1b368ba704794914c875533aed322e8ec1983c
Shweta Kumari Shah <157615234+shwetashah791@users.noreply.github.com>
2025-01-14 19:28:39 +0530

    Time: 39 ms (81.18%), Space: 113.1 MB (25.95%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,41 +1,34 @@
 class RandomizedSet {
+public: 
+RandomizedSet(){
+
+}
+bool insert(int val){
+    if(d.count(val)){
+        return false;
+    }
+    d[val]=q.size();
+    q.push_back(val);
+    return true;
+}
+bool remove(int val){
+    if(!d.count(val)){
+        return false;
+    }
+    int i=d[val];
+    d[q.back()]=i;
+    q[i]=q.back();
+    q.pop_back();
+    d.erase(val);
+    return true;
+}
+int getRandom(){
+    return q[rand()% q.size()];;
+}
 private:
-    std::vector<int> nums;
-    std::unordered_map<int, int> numToIndex;
-
-public:
-    RandomizedSet() {
-        srand(time(0));
-    }
-
-    bool insert(int val) {
-        if (numToIndex.find(val) != numToIndex.end()) {
-            return false;
-        }
-        numToIndex[val] = nums.size();
-        nums.push_back(val);
-        return true;
-    }
-
-    bool remove(int val) {
-        if (numToIndex.find(val) == numToIndex.end()) {
-            return false;
-        }
-        int index = numToIndex[val];
-        int last = nums.back();
-        nums[index] = last;
-        numToIndex[last] = index;
-        nums.pop_back();
-        numToIndex.erase(val);
-        return true;
-    }
-
-    int getRandom() {
-        return nums[rand() % nums.size()];
-    }
+unordered_map<int,int>d;
+vector<int> q;
 };
-
-
 /**
  * Your RandomizedSet object will be instantiated and called as such:
  * RandomizedSet* obj = new RandomizedSet();
