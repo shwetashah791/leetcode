3a90fef40f0298158bad2354f7310cf8913e0c5a
Shweta Kumari Shah <157615234+shwetashah791@users.noreply.github.com>
2025-01-21 10:56:32 +0530

    Time: 3 ms (59.86%), Space: 10.5 MB (83.86%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string>stack;
+        stringstream ss(path);
+        string token;
+        while(getline(ss,token,'/')){
+            if(token==""|| token=="."){
+                continue;
+            }
+            else if(token==".."){
+                if(!stack.empty()){
+                    stack.pop_back();
+                }
+            }else{
+                stack.push_back(token);
+            }
+        }
+        string result="/";
+        for(size_t i=0;i<stack.size();++i){
+            result+=stack[i];
+            if(i<stack.size()-1){
+                result+="/";
+            }
+        }
+        return result;
+}
+};
